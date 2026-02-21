0a598bc2af556b6a90a0983094d3eb5ab3adbd67
tarourou <niranito@outlook.jp>
2026-02-21 15:45:56 +0900

    Add UIs and DOTween

--- Assets/Hashimoto/Scripts/GameManager.cs
+++ Assets/Hashimoto/Scripts/GameManager.cs
@@ -1,14 +1,23 @@
 using System.Threading;
 using UnityEngine;
 using UnityEngine.SocialPlatforms.Impl;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
     public static GameManager instance;
-    public int score;
-    public float timer;
-    public int combo;
-    public float feverTime;
+    [HideInInspector] public int score;
+    [HideInInspector] public float timer;
+    [HideInInspector] public int combo;
+    [HideInInspector] public float feverTimer;
+    Text scoreText, timerText;
+    Image comboImage;
+    [SerializeField] int maxCombo, TimeLimit, feverTime;
+    [SerializeField] Sprite[] comboSprites = new Sprite[11];
+    GameObject
+    public bool isStartGame, isResult;
+
+    Image feverImage;
 
     private void Awake()
     {
@@ -25,41 +34,84 @@ public class GameManager : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        timer = 0;
-        score = 0;
-        combo = 0;
+        feverImage = GameObject.Find("FeverImage").GetComponent<Image>();
+        ResetGame();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(timer >= 0)
+        if(timer > 0)
         {
             timer -= Time.deltaTime;
         }
         else
         {
+            if(isStartGame)
+            {
+                isResult = true;
+            }
             timer = 0;
         }
+        if (feverTimer > 0)
+        {
+            feverTimer -= Time.deltaTime;
+            feverImage.color = Color.red;
+        }
+        else
+        {
+            feverTimer = 0;
+            feverImage.color = new Color(0, 0, 0, 0);
+        }
+        if (combo == maxCombo)
+        {
+            feverTimer = feverTime;
+            combo = 0;
+        }
+        SetText();
     }
     public void ResetGame()
     {
+        scoreText = GameObject.Find("ScoreText").GetComponent<Text>();
+        timerText = GameObject.Find("TimerText").GetComponent<Text>();
+        comboImage = GameObject.Find("ComboImage").GetComponent<Image>();
         score = 0;
-        timer = 30;
+        timer = 0;
         combo = 0;
+        SetText();
     }
-    public void AddScore(int plusScore)
+    public void AddScore()
     {
-        combo += 1;
-        score += plusScore;
+        if(timer > 0)
+        {
+            if (feverTimer == 0)
+            {
+                score += 1;
+                combo += 1;
+            }
+            else if (feverTimer > 0)
+            {
+                score += 3;
+            }
+        }
     }
     public void StartTimer()
     {
         timer = 30;
+        isStartGame = true;
+        isResult = false;
     }
 
     public void Miss(bool isWrongDog)
     {
         combo = 0;
+    }
+    public void SetText()
+    {
+        scoreText.text = score.ToString() + "‘K";
+        timerText.text = timer.ToString() + "•b";
+        comboImage.sprite = comboSprites[combo];
+        Debug.Log("Combo : " + combo);
+        GameObject.Find("ComboText").GetComponent<Text>().text = combo.ToString();
     }
 }
